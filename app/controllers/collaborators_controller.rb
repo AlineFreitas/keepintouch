72f5856886a9fa9aeb699b5491d8a3416ba35c61
Aline de Oliveira Freitas <dreamofaline@gmail.com>
2012-06-25 18:22:50 -0300

    Finished basic layout and started the collaborators controller

---
+++ app/controllers/collaborators_controller.rb
@@ -0,0 +1,17 @@
+class CollaboratorsController < ApplicationController
+  def new
+    @collaborator = Collaborator.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,52 +3,38 @@ require 'spec_helper'
 describe "StaticPages" do
   let(:base_title) { "Keepintouch.net.br -"}
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Seja Bem-Vindo'" do
-      visit '/static_pages/home'
-      page.should have_content('Seja Bem-Vindo')
-    end
+    before { visit root_path }
 
-   it "should have the h1 'Seja Bem-Vindo'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Seja Bem-Vindo')
-    end
+    it { should have_selector('h1',
+                              :text => 'Seja Bem-Vindo') }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Keepintouch.net.br")
-    end
+    it { should have_selector('title',
+                              :text => full_title('')) }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '- Home')
-    end
+    it { should_not have_selector('title',
+                                  :text => '- Home') }
   end
 
   describe "About page" do
-    it "should have the content 'Sobre o Keepintouch'" do
-      visit '/static_pages/about'
-      page.should have_content('Sobre o Keepintouch')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "#{base_title} About")
-    end
+    it { should have_selector('h1',
+                              :text => "Sobre o Keepintouch") }
+
+    it { should have_selector('title',
+                              :text => full_title('About')) }
   end
 
   describe "Help page" do
-    it "should have the content 'Perguntas Frequentes'" do
-      visit '/static_pages/help'
-      page.should have_content('Perguntas Frequentes')
-    end
+    before { visit help_path }
 
-    it "should have the title 'FAQ'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "#{base_title} FAQ")
-    end
+    it { should have_selector('h1',
+                              :text => "Perguntas Frequentes") }
+
+    it {should have_selector('title',
+                             :text => full_title('FAQ')) }
   end
 end

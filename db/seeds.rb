d42cc8e11e78c76b1f9f598ebcf5efeb51e1e5be
Aline de Oliveira Freitas <dreamofaline@gmail.com>
2012-07-01 16:13:35 -0300

    Updated db with a default user for Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+collaborator = Collaborator.create(name: 'Marcus Paes', email: 'paesms@hotmail.com', password: 'rat26600', password_confirmation: 'rat26600')
+collaborator.toggle!(:admin)

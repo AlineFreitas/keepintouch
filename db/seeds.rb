60a62c74e7841b6f3fe1cc5aaa3a310d74bee33d
Aline de Oliveira Freitas <dreamofaline@gmail.com>
2012-07-06 14:06:52 -0300

    Updated seeds.rb to match the required fields for collaborators

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-collaborator = Collaborator.create(name: 'Marcus Paes', email: 'paesms@hotmail.com', fone1: '98331076', password: 'rat26600', password_confirmation: 'rat26600')
+collaborator = Collaborator.create(name: 'Marcus Paes', gender: 'Homem', email: 'paesms@hotmail.com', birth_date: '29/07/1956', street: 'Avenida Alberto Torres', number: 266, hood: 'Centro', cep: '00000000', fone1: '98331076', password: 'rat26600', password_confirmation: 'rat26600')
 collaborator.toggle!(:admin)
